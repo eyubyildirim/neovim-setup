29e6bcbc8b862de67579f15ace357330b293f53b
eyubyildirim <eyub2001@gmail.com>
2025-03-16 20:08:22 +0300

    fix: debug

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -1,6 +1,7 @@
 return {
   'mfussenegger/nvim-dap',
   opts = {},
+  event = 'InsertEnter',
   config = function()
     vim.keymap.set('n', '<Leader>dc', function()
       require('dap').continue()
