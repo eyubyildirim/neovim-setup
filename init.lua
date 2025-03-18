163d98383c1e57555cd02814626e4bba3ea475a6
eyubyildirim <eyub2001@gmail.com>
2025-03-18 18:34:37 +0300

    feat: add fugitive

--- init.lua
+++ init.lua
@@ -1086,6 +1086,11 @@ require('lazy').setup({
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
+  {
+    'tpope/vim-fugitive',
+    opts = {},
+    config = function() end,
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
